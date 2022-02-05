60deb19d892e014af4627b7838c82f3ed8d9a8e7
seed <hsik0225@gmail.com>
2022-02-05 17:37:41 +0900

    feat: init project

---
+++ src/test/java/com/seed/autotest/AutoTestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.seed.autotest;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
